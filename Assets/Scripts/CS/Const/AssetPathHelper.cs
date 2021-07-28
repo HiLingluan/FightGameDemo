be668744dc82d50e6651028c84f0a197f058e722
LingLuan <656897516@qq.com>
2021-07-28 14:15:57 +0800

    增加Lua启动流程

---
+++ Assets/Scripts/CS/Const/AssetPathHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AssetPathHelper 
+{
+
+    public readonly static string streamingAssetsPath = Application.streamingAssetsPath;
+    public readonly static string persistentDataPath = Application.persistentDataPath;
+    public readonly static string temporaryCachePath = Application.temporaryCachePath;
+    public readonly static string dataPath = Application.dataPath;
+
+    #region lua resource path
+    public readonly static string BuildInLuaPath = dataPath + "/Scripts/Lua/";
+    public readonly static string LuaFolderPath = persistentDataPath + "/Lua/";
+    public readonly static string mainLuaPath = BuildInLuaPath + "Main.lua";
+    public readonly static string disposeLuaPath = BuildInLuaPath + "Dispose.lua";
+    #endregion
+}
---
+++ Assets/Scripts/Lua/Main.lua
@@ -0,0 +1,22 @@
+
+function Awake()
+    print("Main============= Awake")
+end
+
+function Start()
+    print("Main============= Start")
+end
+
+function Update()
+    
+end
+
+function LateUpdate()
+    
+end
+
+function OnDestroy()
+    
+end
+
+
