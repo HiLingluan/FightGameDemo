f15ba71a3887306207a09d61fb2e45d2d74f5c04
凌乱 <656897516@qq.com>
2021-10-13 21:35:17 +0800

    增加游戏地图,格子类,今天开始继续写0.0

---
+++ Assets/Scripts/CS/Logic/Game/Map/MapGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 游戏地图管理
+/// </summary>
+public class GameMap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
